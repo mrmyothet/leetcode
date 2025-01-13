e2f780860e266eeb943723fc31b9eeb2265eb232
Myo Thet <mr.myothet@gmail.com>
2025-01-13 18:47:28 +0700

    1929. Concatenation of Array

---
+++ LeetCode.Arrays/GetConcatenation.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Arrays;
+
+public class GetConcatenation
+{
+    public static int[] GetConcatenationArray(int[] nums)
+    {
+        int length = nums.Length * 2;
+        int[] result = new int[length];
+        int index = 0;
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                result[index] = nums[j];
+                index++;
+            }
+        }
+        return result;
+    }
+}
---
+++ LeetCode.ArraysTests/GetConcatenationTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Arrays;
+
+namespace LeetCode.ArraysTests;
+
+public class GetConcatenationTests
+{
+    [Test]
+    public void Test_Example_1()
+    {
+        int[] nums = [1, 2, 1];
+        int[] expected = [1, 2, 1, 1, 2, 1];
+
+        int[] result = GetConcatenation.GetConcatenationArray(nums);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Example_2()
+    {
+        int[] nums = [1, 3, 2, 1];
+        int[] expected = [1, 3, 2, 1, 1, 3, 2, 1];
+
+        int[] result = GetConcatenation.GetConcatenationArray(nums);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
