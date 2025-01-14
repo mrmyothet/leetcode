54d2673d1eca9a90b4be649f3d52c46b76ed8d1f
Myo Thet <mr.myothet@gmail.com>
2025-01-14 13:14:18 +0700

    2390. Removing Stars From a String

---
+++ LeetCode.Medium/removeStars.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Medium;
+
+public class removeStars
+{
+    public static string solution(string input)
+    {
+        string result = "";
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == '*')
+            {
+                result = result.Substring(0, result.Length - 1);
+            }
+            else
+            {
+                result = result += input[i].ToString();
+            }
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode.MediumTests/removeStarsTests.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.MediumTests;
+
+using LeetCode.Medium;
+
+[TestFixture]
+public class removeStarsTests
+{
+    [SetUp]
+    public void Setup() { }
+
+    [Test]
+    public void Test_Example_1()
+    {
+        string input = "leet**cod*e";
+        string expected = "lecoe";
+
+        string result = removeStars.solution(input);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Example_2()
+    {
+        string input = "erase*****";
+        string expected = "";
+
+        string result = removeStars.solution(input);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
