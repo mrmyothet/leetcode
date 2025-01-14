07a47986bc0c88e91a32fe27c1bb094bc3f362c3
Myo Thet <mr.myothet@gmail.com>
2025-01-14 14:18:19 +0700

    use char stack instead of substring

--- LeetCode.Medium/removeStars.cs
+++ LeetCode.Medium/removeStars.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -9,22 +10,50 @@ namespace LeetCode.Medium;
 
 public class removeStars
 {
+    // Time Limit Exceeded - ERROR in leetcode
+    //public static string solution(string input)
+    //{
+    //    string result = "";
+
+    //    for (int i = 0; i < input.Length; i++)
+    //    {
+    //        if (input[i] == '*')
+    //        {
+    //            result = result.Substring(0, result.Length - 1);
+    //        }
+    //        else
+    //        {
+    //            result = result += input[i].ToString();
+    //        }
+    //    }
+
+    //    return result;
+    //}
+
     public static string solution(string input)
     {
         string result = "";
+        Stack<char> charStack = new Stack<char>();
 
         for (int i = 0; i < input.Length; i++)
         {
-            if (input[i] == '*')
+            char element = input[i];
+            if (charStack is not null && element == '*')
             {
-                result = result.Substring(0, result.Length - 1);
+                charStack.Pop();
             }
             else
             {
-                result = result += input[i].ToString();
+                charStack.Push(element);
             }
         }
 
-        return result;
+        Stack<char> reverseStack = new Stack<char>();
+        while (charStack.Count > 0)
+        {
+            reverseStack.Push(charStack.Pop());
+        }
+
+        return String.Join("", reverseStack);
     }
 }
